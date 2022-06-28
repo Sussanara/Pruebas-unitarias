210dcd336a56204d608fd3358e5ff2698d5fccf9
Sussanara <sussanaracely@gmail.com>
2022-06-28 15:41:03 +0000

    todos los test

--- test.js
+++ test.js
@@ -5,12 +5,17 @@ let total = sum(14, 9);
 expect(total).toBe(23);
 });
 
-test ('Pasar de 1 dóllar a Yen',() => {
+test ('Pasar de 1 Dollar a Yen',() => {
 let total = fromDollarToYen(1.2);
 expect (total).toBe(127.9);
 });
 
-test ('Pasar de1 Yen a Pound',() => {
+test ('Pasar de 1 Yen a Pound',() => {
     let total = fromYenToPound(127.9);
     expect (total).toBe(0.8);
+});
+
+test ('Pasar de 1 Euro a Dollar',() => {
+    let total = fromEuroToDollar(1.2);
+    expect (total).toBe(1.44);
 });
